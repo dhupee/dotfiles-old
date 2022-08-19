9bdeebdb5bfc4e31c04561d763253d17c2b77106
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2022-08-19 11:22:53 +0700

    add bottles config and editing install sh

--- dot_scripts/install.sh
+++ dot_scripts/install.sh
@@ -11,14 +11,14 @@ sudo dnf install git-all
 sudo dnf install python3.9
 
 # install github cli
-sudo dnf config-manager \ 
+sudo dnf config-manager \
     --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
 sudo dnf install gh
 
 # install zsh and ohmyzsh
 sudo dnf install zsh
 chsh -s $(which zsh)
-sh -c \ 
+sh -c \
     "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # install ohmyzsh plugins
@@ -36,7 +36,7 @@ sudo dnf install brave-browser
 
 # install vscode
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c \ 
+sudo sh -c \
     'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 dnf check-update
@@ -55,8 +55,8 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 # install spotify and discord
 sudo flatpak install flathub \
-    com.spotify.Client \ 
-    com.discordapp.Discord \ 
+    com.spotify.Client \
+    com.discordapp.Discord \
     com.usebottles.bottles
 
 #-----------------------------INSTALL APPIMAGES------------------------
