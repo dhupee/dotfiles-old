997e3e49549cfb7651b04cf0f2e39cf62ff921f8
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2022-08-30 20:30:59 +0700

    auto update

--- dot_scripts/chezmoi-save.sh
+++ dot_scripts/chezmoi-save.sh
@@ -4,19 +4,24 @@
 
 dir_array=(
     "$HOME/.backup_bottles/"
-    "$HOME/.backup_nothing/"
+    "$HOME/.scripts/"
+    "$HOME/.config/htop/"
 )
 
 file_array=(
     "$HOME/.zshrc"
-    "$HOME/.anyfile"
+    "$HOME/.gitconfig"
+    "$HOME/.p10k.zsh"
+    "$HOME/.bashrc"
 )
+
+#----------------------SAVING FILES AND DIRS----------------------
 
 for d in "${dir_array[@]}";do
     if [ -d $d ];then
                 
-        echo "$d exists"
-        echo ""
+        # echo "$d exists"
+        echo "adding $d"
     else
         echo "$d is not exists"
         read -p "Would you like to make one? (y/n)?" choice
@@ -25,21 +30,26 @@ for d in "${dir_array[@]}";do
         n|N ) echo "skipping";;
         * ) echo "invalid";;
         esac
+
     fi
+    chezmoi add $d
 done
 
 for f in "${file_array[@]}";do
     if [ -f $f ];then
-        echo "$f exists"
-        echo ""
+        echo "adding $f"
     else
         echo "$f is not exists"
         echo ""
     fi
+
+    chezmoi add $f
 done
 
-# # pushing to github
-# chezmoi cd
-# git add .
-# git commit -m "auto update"
-# git push
+#---------------------------COMMIT TO GIT----------------------
+
+# pushing to github
+cd $HOME/.local/share/chezmoi
+git add .
+git commit -m "auto update"
+git push
