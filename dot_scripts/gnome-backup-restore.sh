cb085f2e1eef3b64f308b4bfcc76e1fba5e6be64
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2022-09-21 14:53:55 +0700

    auto update

--- dot_scripts/gnome-backup-restore.sh
+++ dot_scripts/gnome-backup-restore.sh
@@ -1,4 +1,15 @@
 #!bin/bash
 
+# need ARG setting for restore
+
 # backup gnome-shell
 dconf dump /org/gnome/shell/ > .gnome-backup/gnome-shell
+
+# backup gnome-desktop
+dconf dump /org/gnome/desktop/ > .gnome-backup/gnome-desktop
+
+# backup settings-daemon
+dconf dump /org/gnome/settings-daemon/ > .gnome-backups/settings-daemon
+
+# backup freedesktop (for now it's just for anthy)
+dconf dump /org/freedesktop/ > .gnome-backups/freedesktop
