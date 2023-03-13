4c04fc4967871e5c997af6ee8022ede257d94f56
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2023-03-13 23:55:46 +0700

    automated update by dhupee

---
+++ dot_scripts/remove-orphans.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# List all orphaned packages
+orphans=$(pacman -Qdt)
+
+# If there are no orphaned packages, exit
+if [[ -z "$orphans" ]]; then
+  echo "No orphaned packages."
+  exit 0
+fi
+
+# Count number of orphaned packages
+count=$(echo "$orphans" | wc -l)
+
+# Print list of orphaned packages with count
+echo "There are $count orphaned packages:"
+echo "$orphans"
+
+# Prompt user to remove orphaned packages
+read -p "Do you want to remove these packages? [y/N] " choice
+
+case "$choice" in
+  y|Y )
+    # Remove orphaned packages
+    sudo pacman -Rns $orphans
+    ;;
+  * )
+    echo "Package removal cancelled."
+    exit 0
+    ;;
+esac
