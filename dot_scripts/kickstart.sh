b9edcde807de931795fcabcfa1a90998eeb059b9
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2023-07-27 09:31:39 +0700

    automated update by dhupee

--- dot_scripts/kickstart.sh
+++ dot_scripts/kickstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install the needed programs
-curl -fsSL https://raw.githubusercontent.com/dhupee/dotfiles/master/dot_scripts/install-arch.sh | sudo bash
+curl -fsSL https://raw.githubusercontent.com/dhupee/dotfiles/master/dot_scripts/install-arch.sh | bash
 
 # Clone your dotfiles repository using Chezmoi
 chezmoi init --apply dhupee
