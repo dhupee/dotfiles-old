48c8360a09e109d84cbe034fae0d6b042f7b5bcc
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2023-08-29 10:50:16 +0700

    automated update by dhupee, at 10:50 29/08/2023

--- private_dot_config/nvim/lua/user/plugins/community.lua
+++ private_dot_config/nvim/lua/user/plugins/community.lua
@@ -3,7 +3,9 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
+  { import = "astrocommunity.colorscheme.nord-nvim"},
+  { import = "astrocommunity.completion.cmp-cmdline"},
+  { import = "astrocommunity.completion.codeium-vim"},
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
