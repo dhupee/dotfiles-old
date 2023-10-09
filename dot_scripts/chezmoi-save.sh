6847e0e65c549ad7c5cb14dbfdabf8e83f70e812
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2023-10-09 11:23:04 +0700

    automated update by dhupee, at 11:23 09/10/2023

--- dot_scripts/chezmoi-save.sh
+++ dot_scripts/chezmoi-save.sh
@@ -86,8 +86,9 @@ if [ "$1" != "--no-rclone" ]; then
     if [ -d "$HOME/.lazerexport" ]; then
         read -p "Would you like to backup this folder? (y/n)?" choice
         case "$choice" in 
-        y|Y ) echo "yes, backing up lazerexport folder" && rclone copy "$HOME/.lazerexport" mega-dh:akago.lazerexport -P ;;
+        y|Y ) echo "yes, backing up lazerexport folder" && rclone copy "$HOME/.lazerexport" mega-dh:akago.lazerexport -P;;
         n|N ) echo "skipping";;
         * ) echo "choice is invalid";;
+        esac
     fi
 fi
