b85c3d4bd1da7043332d2bc4f42217646a270a5e
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2024-02-01 08:47:56 +0700

    automated update by dhupee, at 08:47 01/02/2024

---
+++ dot_aliases/tmux.sh
@@ -0,0 +1,11 @@
+# Sessions
+alias tsa="tmux attach -t"
+alias tsd="tmux detach -t"
+alias tsk="tmux kill-session -t"
+alias tsn="tmux new-session -t"
+alias tsl="tmux list-sessions"
+alias tsr="tmux rename-session"
+
+# Windows
+alias twl="tmux list-windows"
+alias twk="tmux kill-window"
