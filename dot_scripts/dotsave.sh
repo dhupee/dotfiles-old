fba9b06e36c2c1a341494b2bfb639cf1a6e421d7
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2024-02-02 12:57:59 +0700

    automated update by dhupee, at 12:57 02/02/2024

--- dot_scripts/dotsave.sh
+++ dot_scripts/dotsave.sh
@@ -5,67 +5,68 @@
 rclone_remote="mega-dh"
 
 dir_array=(
-    "$HOME/.backup-bottles/"
-    "$HOME/.scripts/"
-    "$HOME/.config/htop/"
-    "$HOME/.config/btop/"
-    "$HOME/.config/neofetch/"
-    "$HOME/Wallpapers/"
-    "$HOME/.fonts/"
-    "$HOME/.completion_zsh/"
-    "$HOME/.config/micro/"
-    "$HOME/.config/nvim/"
-    "$HOME/.config/nvchad.bak/"
-    "$HOME/.config/PrusaSlicer/"
-    "$HOME/.config/obs-studio/basic/"
-    "$HOME/.config/containers/"
-    "$HOME/.aliases/"
-    "$HOME/.config/helix/"
-    "$HOME/.config/BetterDiscord/plugins"
-    "$HOME/.config/BetterDiscord/themes"
+	"$HOME/.backup-bottles/"
+	"$HOME/.scripts/"
+	"$HOME/.config/htop/"
+	"$HOME/.config/btop/"
+	"$HOME/.config/neofetch/"
+	"$HOME/Wallpapers/"
+	"$HOME/.fonts/"
+	"$HOME/.completion_zsh/"
+	"$HOME/.config/micro/"
+	"$HOME/.config/nvim/"
+	"$HOME/.config/nvchad.bak/"
+	"$HOME/.config/PrusaSlicer/"
+	"$HOME/.config/obs-studio/basic/"
+	"$HOME/.config/containers/"
+	"$HOME/.aliases/"
+	"$HOME/.config/helix/"
+	"$HOME/.config/BetterDiscord/plugins/"
+	"$HOME/.config/BetterDiscord/themes/"
+	"$HOME/.config/lobster/"
 )
 
 file_array=(
-    "$HOME/.zshrc"
-    "$HOME/.zshrc.pre-oh-my-zsh"
-    "$HOME/.gitconfig"
-    "$HOME/.p10k.zsh"
-    "$HOME/.bashrc"
-    "$HOME/.nanorc"
-    "$HOME/.fonts.conf"
-    "$HOME/.tmux.conf"		
-    "$HOME/.config/spicetify/config-xpui.ini"
-    "$HOME/.config/obs-studio/global.ini"
+	"$HOME/.zshrc"
+	"$HOME/.zshrc.pre-oh-my-zsh"
+	"$HOME/.gitconfig"
+	"$HOME/.p10k.zsh"
+	"$HOME/.bashrc"
+	"$HOME/.nanorc"
+	"$HOME/.fonts.conf"
+	"$HOME/.tmux.conf"
+	"$HOME/.config/spicetify/config-xpui.ini"
+	"$HOME/.config/obs-studio/global.ini"
 )
 
 #----------------------SAVING FILES AND DIRS----------------------
 
-for d in "${dir_array[@]}";do
-    if [ -d $d ];then
-        # echo "$d exists"
-        echo "adding $d"
-    else
-        echo "$d is not exists"
-        read -p "Would you like to make one? (y/n)?" choice
-        case "$choice" in 
-        y|Y ) echo "yes, creating directory $d" && mkdir $d;;
-        n|N ) echo "skipping";;
-        * ) echo "choice is invalid";;
-        esac
+for d in "${dir_array[@]}"; do
+	if [ -d $d ]; then
+		# echo "$d exists"
+		echo "adding $d"
+	else
+		echo "$d is not exists"
+		read -p "Would you like to make one? (y/n)?" choice
+		case "$choice" in
+		y | Y) echo "yes, creating directory $d" && mkdir $d ;;
+		n | N) echo "skipping" ;;
+		*) echo "choice is invalid" ;;
+		esac
 
-    fi
-    chezmoi add $d
+	fi
+	chezmoi add $d
 done
 
-for f in "${file_array[@]}";do
-    if [ -f $f ];then
-        echo "adding $f"
-    else
-        echo "$f is not exists"
-        echo ""
-    fi
+for f in "${file_array[@]}"; do
+	if [ -f $f ]; then
+		echo "adding $f"
+	else
+		echo "$f is not exists"
+		echo ""
+	fi
 
-    chezmoi add $f
+	chezmoi add $f
 done
 
 echo " "
@@ -93,33 +94,33 @@ rclone copy "$HOME/.config/transmission/" $rclone_remote:akago/.config/transmiss
 
 # if no-konsave flag then dont save konsave profiles
 if [ "$1" != "--no-rclone" ]; then
-    # pushing the big files to mega
-    echo " "
-    echo "Pushing konsave profiles to Mega"
+	# pushing the big files to mega
+	echo " "
+	echo "Pushing konsave profiles to Mega"
 
-    # NOTE: make sure to have same folder name, I want to try automate the backup method
+	# NOTE: make sure to have same folder name, I want to try automate the backup method
 
-    # Saving konsave profiles
-    # rclone delete $rclone_remote:akago/konsave-profiles/
-    rclone copy "$HOME/.konsave-profiles/" $rclone_remote:akago/.konsave-profiles/ -P
+	# Saving konsave profiles
+	# rclone delete $rclone_remote:akago/konsave-profiles/
+	rclone copy "$HOME/.konsave-profiles/" $rclone_remote:akago/.konsave-profiles/ -P
 
-    # MAKE THIS FOR VERY HUGE STORAGE
-    # if folder lazerexport exist, ask to backup or not
-    # don't delete, append since the contain of the beatmap didn't change like others
-    if [ -d "$HOME/lazerexport/" ]; then
-        read -p "Would you like to backup this folder? (y/n)?" choice
-        case "$choice" in 
-        y|Y ) echo "yes, backing up lazerexport folder"  && rclone copy "$HOME/lazerexport/" mega-dh:akago/lazerexport/ -P;;
-        n|N ) echo "skipping";;
-        * ) echo "choice is invalid";;
-        esac
-        
-        # ask to delete lazerexport locally
-        read -p "Would you like to delete lazerexport folder? (y/n)?" choice
-        case "$choice" in
-        y|Y ) echo "yes, deleting lazerexport folder" && rm -rf "$HOME/lazerexport/";;
-        n|N ) echo "skipping";;
-        * ) echo "choice is invalid";;
-        esac
-    fi
+	# MAKE THIS FOR VERY HUGE STORAGE
+	# if folder lazerexport exist, ask to backup or not
+	# don't delete, append since the contain of the beatmap didn't change like others
+	if [ -d "$HOME/lazerexport/" ]; then
+		read -p "Would you like to backup this folder? (y/n)?" choice
+		case "$choice" in
+		y | Y) echo "yes, backing up lazerexport folder" && rclone copy "$HOME/lazerexport/" mega-dh:akago/lazerexport/ -P ;;
+		n | N) echo "skipping" ;;
+		*) echo "choice is invalid" ;;
+		esac
+
+		# ask to delete lazerexport locally
+		read -p "Would you like to delete lazerexport folder? (y/n)?" choice
+		case "$choice" in
+		y | Y) echo "yes, deleting lazerexport folder" && rm -rf "$HOME/lazerexport/" ;;
+		n | N) echo "skipping" ;;
+		*) echo "choice is invalid" ;;
+		esac
+	fi
 fi
