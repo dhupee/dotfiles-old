7095acedeb5057282e2f709fea5570a072e6fa0f
narutohaj00@gmail.com <32934122+dhupee@users.noreply.github.com>
2024-02-20 04:43:43 +0700

    automated update by dhupee, at 04:43 20/02/2024

--- private_dot_config/nvim/lua/plugins/mason.lua
+++ private_dot_config/nvim/lua/plugins/mason.lua
@@ -1,25 +1,29 @@
 return {
   {
     "williamboman/mason.nvim",
-    cmd = {
-      "Mason",
-      "MasonInstall",
-      "MasonUninstall",
-      "MasonUninstallAll",
-      "MasonLog",
-      "MasonUpdate", -- AstroNvim extension here as well
-      "MasonUpdateAll", -- AstroNvim specific
-    },
     opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_uninstalled = "✗",
-          package_pending = "⟳",
-        },
+      ensure_installed = {
+        "arduino-language-server",
+        "clangd",
+        "cpplint",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "gofumpt",
+        "goimports",
+        "gopls",
+        "hadolint",
+        "json-lsp",
+        "lua-language-server",
+        "markdownlint",
+        "marksman",
+        "prettier",
+        "pyright",
+        "ruff-lsp",
+        "shfmt",
+        "stylua",
+        "tailwindcss-language-server",
+        "yaml-language-server",
       },
     },
-    build = ":MasonUpdate",
-    config = require "plugins.configs.mason",
   },
 }
